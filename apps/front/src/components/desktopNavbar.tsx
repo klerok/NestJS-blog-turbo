82ef94e2d2b415ce6171911e0fe86759b446dd07
klerok <klerikdeezaster@gmail.com>
2026-04-19 16:28:27 +0300

    navbar

---
+++ apps/front/src/components/desktopNavbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { PropsWithChildren, useEffect, useState } from "react";
+
+type Props = PropsWithChildren;
+
+const DesktopNavbar = (props: Props) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const isScrolledDown = scrollPosition > 10;
+  return (
+    <nav
+      className={cn("fixed w-full z-30 text-white top-0 block", {
+        "bg-white text-gray-700 shadow-md": isScrolledDown,
+      })}
+    >
+      <div className="flex items-center px-4 py-4 container">
+        {props.children}
+      </div>
+      <hr className="border-b border-gray-100 opacity-25"/>
+    </nav>
+  );
+};
+
+export default DesktopNavbar;
