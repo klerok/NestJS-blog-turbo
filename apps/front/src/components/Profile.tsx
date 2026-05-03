f3ce1268e29347fb6f1fc86b985a4adcf2686cff
klerok <klerikdeezaster@gmail.com>
2026-05-03 14:57:15 +0300

    profile feature

---
+++ apps/front/src/components/Profile.tsx
@@ -0,0 +1,53 @@
+import { SessionUser } from "@/lib/session";
+import { Popover } from "@radix-ui/react-popover";
+import { PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Avatar } from "@radix-ui/react-avatar";
+import { AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  ArrowRightStartOnRectangleIcon,
+  ListBulletIcon,
+  PencilSquareIcon,
+  UserIcon,
+} from "@heroicons/react/20/solid";
+import Link from "next/link";
+
+type Props = {
+  user: SessionUser;
+};
+
+const Profile = ({ user }: Props) => {
+  return (
+    <Popover>
+      <PopoverTrigger className="cursor-pointer">
+        <Avatar>
+          <AvatarImage src={user.avatar} />
+          <AvatarFallback>
+            <UserIcon className="w-8 text-slate-500" />
+          </AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="flex justify-center items-center gap-3">
+          <UserIcon className="w-4" />
+          <p>{user.name}</p>
+        </div>
+        <div className="*:grid *:grid-cols-5 *:gap-3 *:items-center *:my-2 *:py-2 [&>*>span]:col-span-4 [&>*:hover]:bg-sky-500 [&>*:hover]:text-white *:transition *:rounded-md [&>*>*:nth-child(1)]:justify-self-end">
+          <a href="/api/auth/signout">
+            <ArrowRightStartOnRectangleIcon className="w-4" />
+            <span>Sign Out</span>
+          </a>
+          <Link href="/user/create-post">
+            <PencilSquareIcon className="w-4" />
+            <span>Create new Post</span>
+          </Link>
+          <Link href="/user/my-posts">
+            <ListBulletIcon className="w-4" />
+            <span>My Posts</span>
+          </Link>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default Profile;
--- apps/front/src/components/navbar.tsx
+++ apps/front/src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "@/lib/session";
 import Link from "next/link";
 import SignInPanel from "./signinPanel";
+import Profile from "./Profile";
 
 type Props = {};
 
@@ -9,7 +10,7 @@ const Navbar = async (props: Props) => {
   return (
     <>
       <h1 className="text-2xl font-bold p-2">My Modern Blog</h1>
-      <div className="flex gap-2 ml-auto [&>a]:py-2 [&>a]:px-4 [&>a]:transition [&>a]:rounded-md [&>a:hover]:text-sky-100 [&>a:hover]:bg-sky-500">
+      <div className="md:ml-auto  px-2 flex flex-col md:flex-row gap-2 md:items-center md:justify-center [&>a:hover]:bg-sky-500 [&>a:hover]:text-sky-100 [&>a]:rounded-md [&>a]:transition [&>a]:duration-200 [&>a]:px-4 md:[&>a]:py-2 [&>a]:py-1 ">
         <Link href="/" className="">
           Blog
         </Link>
@@ -20,7 +21,7 @@ const Navbar = async (props: Props) => {
           Contact
         </Link>
         {session && session.user ? (
-          <a href={"/api/auth/signout"}>Sign Out</a>
+          <Profile user={session.user} />
         ) : (
           <SignInPanel />
         )}
